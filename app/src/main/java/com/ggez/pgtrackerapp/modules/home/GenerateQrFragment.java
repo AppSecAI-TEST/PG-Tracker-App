83c5849064db5d44e4f3a76255e3c736010fb784
Kat <kathleendsantiago@gmail.com>
2017-08-09 19:34:11 +0800

    KDS - fragments

---
+++ app/src/main/java/com/ggez/pgtrackerapp/modules/home/GenerateQrFragment.java
@@ -0,0 +1,31 @@
+package com.ggez.pgtrackerapp.modules.home;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ggez.pgtrackerapp.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by katleen on 8/9/17.
+ */
+public class GenerateQrFragment extends Fragment {
+
+    MainActivity mainActivity;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_fragment_generate_qr, container, false);
+        ButterKnife.bind(this, view);
+        mainActivity = (MainActivity) getActivity();
+        return view;
+
+    }
+}
