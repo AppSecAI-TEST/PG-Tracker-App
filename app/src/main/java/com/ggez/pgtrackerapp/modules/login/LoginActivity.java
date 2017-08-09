5376677e5f76b2a24dd760d1d2e4d8d5c1fd3ca7
omatt <omar.matthew@gmail.com>
2017-08-09 15:58:13 +0800

    Added QR code logic

--- app/src/main/java/com/ggez/pgtrackerapp/modules/login/LoginActivity.java
+++ app/src/main/java/com/ggez/pgtrackerapp/modules/login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.ggez.pgtrackerapp.modules.login;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
@@ -20,9 +21,13 @@ import com.ggez.pgtrackerapp.modules.home.MainActivity;
 import com.ggez.pgtrackerapp.modules.profile.ProfileActivity;
 import com.ggez.pgtrackerapp.modules.register.RegisterActivity;
 import com.ggez.pgtrackerapp.qr.decoder.IntentIntegrator;
+import com.ggez.pgtrackerapp.qr.decoder.IntentResult;
 import com.ggez.pgtrackerapp.utils.Validator;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.inject.Inject;
 
@@ -104,52 +109,65 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
 
     @Override
     public void hideSoftInput(View view) {
-        InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     @OnClick(R.id.btn_login)
     void onClickLogin() {
-        startActivity(new Intent(this, QRActivity.class));
-//        hideSoftInput(btnLogin);
-//        clearTilErrors();
-//        boolean error = false;
-//        if (!Validator.isEmailValid(etEmail.getText().toString())) {
-//            error = true;
-//            setUsernameError();
-//        }
-//        if (!Validator.isPasswordValid(etPassword.getText().toString())) {
-//            error = true;
-//            setPasswordError();
-//        }
-//        if (!error) {
-//            FirebaseAuth.getInstance().signOut();   // sign-out anonymous user
-//            showProgress(getString(R.string.logging_in));
-//            mFirebaseAuth.signInWithEmailAndPassword(etEmail.getText().toString(), etPassword.getText().toString())
-//                    .addOnCompleteListener(this, task -> {
-//                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
-//
-//                        hideProgress();
-//                        if (!task.isSuccessful()) {
-//                            Log.w(TAG, "signInWithEmail:failed", task.getException());
-//                            FirebaseAuth.getInstance().signInAnonymously();   // sign-in failed. sign-in as anonymous user again
-//                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-//                        } else {
-//                            Bundle eventBundle = new Bundle();
-//                            eventBundle.putString(FirebaseAnalytics.Param.SIGN_UP_METHOD, "password_auth");
-//                            mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, eventBundle);
-//                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
-//                            startActivity(new Intent(this, MainActivity.class));
-//                            finish();
-//                        }
-//                    });
-//        }
+//        startActivity(new Intent(this, QRActivity.class));
+        hideSoftInput(btnLogin);
+        clearTilErrors();
+        boolean error = false;
+        if (!Validator.isEmailValid(etEmail.getText().toString())) {
+            error = true;
+            setUsernameError();
+        }
+        if (!Validator.isPasswordValid(etPassword.getText().toString())) {
+            error = true;
+            setPasswordError();
+        }
+        if (!error) {
+            FirebaseAuth.getInstance().signOut();   // sign-out anonymous user
+            showProgress(getString(R.string.logging_in));
+            mFirebaseAuth.signInWithEmailAndPassword(etEmail.getText().toString(), etPassword.getText().toString())
+                    .addOnCompleteListener(this, task -> {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        hideProgress();
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            FirebaseAuth.getInstance().signInAnonymously();   // sign-in failed. sign-in as anonymous user again
+                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Bundle eventBundle = new Bundle();
+                            eventBundle.putString(FirebaseAnalytics.Param.SIGN_UP_METHOD, "password_auth");
+                            mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, eventBundle);
+                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(this, MainActivity.class));
+                            finish();
+                        }
+                    });
+        }
     }
 
     @OnClick(R.id.btn_register)
     void onClickRegister() {
         clearTilErrors();
-        new IntentIntegrator(this).initiateScan(-1);
-//        startActivity(new Intent(this, RegisterActivity.class));
+//        new IntentIntegrator(this).initiateScan(-1);
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+            String url = scanResult.getContents();
+            Pattern pattern = Pattern.compile("http://([a-z0-9]*.)example.com");
+            if(!pattern.matcher(url).matches()) Toast.makeText(this, "Invalid QR Code", Toast.LENGTH_SHORT).show();
+            else startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            Log.i(TAG, "getFormatName: " + scanResult.getFormatName() + " getContents: " + url);
+        }
     }
 }
