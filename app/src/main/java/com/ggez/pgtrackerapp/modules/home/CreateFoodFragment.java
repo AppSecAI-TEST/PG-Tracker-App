91d56b33f3235c4e9d531aa5cf2f1a573fdf20e8
Kat <kathleendsantiago@gmail.com>
2017-08-10 10:22:38 +0800

    KDS - UI, create food

--- app/src/main/java/com/ggez/pgtrackerapp/modules/home/CreateFoodFragment.java
+++ app/src/main/java/com/ggez/pgtrackerapp/modules/home/CreateFoodFragment.java
@@ -6,42 +6,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.ggez.pgtrackerapp.R;
-import com.ggez.pgtrackerapp.qr.decoder.IntentIntegrator;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
- * Created by katleen on 8/9/17.
+ * Created by katleen on 8/10/17.
  */
-public class CreateMenuFragment extends Fragment {
+public class CreateFoodFragment extends Fragment {
 
     MainActivity mainActivity;
-
-    @BindView(R.id.btn_add_food)
-    Button tvUserName;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.layout_fragment_create_menu, container, false);
+        View view = inflater.inflate(R.layout.layout_fragment_create_food, container, false);
         ButterKnife.bind(this, view);
         mainActivity = (MainActivity) getActivity();
         return view;
 
     }
-
-    @OnClick(R.id.btn_add_food)
-    void onClickAddFood(){
-        mainActivity.changeFragment(new CreateFoodFragment(),false);
-    }
-
-
-
 }
