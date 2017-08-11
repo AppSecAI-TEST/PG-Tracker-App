d82ee9f4e3e84871b8f46e1a53b1a36471ca0288
omatt <omar.matthew@gmail.com>
2017-08-11 09:32:20 +0800

    Removed unused missing dependencies

--- app/src/main/java/com/ggez/pgtrackerapp/modules/home/UserHistoryFragment.java
+++ app/src/main/java/com/ggez/pgtrackerapp/modules/home/UserHistoryFragment.java
@@ -4,45 +4,27 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.ggez.pgtrackerapp.R;
-import com.ggez.pgtrackerapp.models.Food;
 import com.ggez.pgtrackerapp.utils.Constants;
-import com.ggez.pgtrackerapp.utils.Utils;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 import java.util.Locale;
-import java.util.TimeZone;
-
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * Created by katleen on 8/9/17.
@@ -82,27 +64,21 @@ public class UserHistoryFragment extends Fragment {
         SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
         String dateToday = df.format(myCalendar.getTime());
 
-        etDate = (EditText) view.findViewById(R.id.et_date);
+        etDate = view.findViewById(R.id.et_date);
         etDate.setText(dateToday);
 
-        ibSelectDate = (ImageButton) view.findViewById(R.id.ib_select_date);
-        ibSelectDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onclick date");
-                new DatePickerDialog(mainActivity, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        myCalendar.set(Calendar.YEAR, year);
-                        myCalendar.set(Calendar.MONTH, monthOfYear);
-                        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                        updateList();
-                    }
-                }, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-            }
+        ibSelectDate = view.findViewById(R.id.ib_select_date);
+        ibSelectDate.setOnClickListener(view1 -> {
+            Log.d(TAG, "onclick date");
+            new DatePickerDialog(mainActivity, (view11, year, monthOfYear, dayOfMonth) -> {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateList();
+            }, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
         });
 
-        lvFoodHistory = (ListView) view.findViewById(R.id.lv_food_history);
+        lvFoodHistory = view.findViewById(R.id.lv_food_history);
 
 
 
